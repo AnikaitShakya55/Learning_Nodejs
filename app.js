6e789f48221a7acb6a5cf8a81eb1d38e7df11a62
Akshxar <shakya.ani47@gmail.com>
2024-07-02 21:09:49 +0530

    part 3 Module Exports

---
+++ app.js
@@ -0,0 +1,5 @@
+const {add,sub}= require('./file')
+
+console.log(add(1,2))
+
+console.log(add(5,2))
--- file.js
+++ file.js
@@ -1,1 +1,15 @@
-console.log("hello")
+function add(a,b) {
+
+    return a+b;
+    
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+
+module.exports = {
+    add : add,
+    sub :sub,
+}
