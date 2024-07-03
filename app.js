f27f62a49a754eda87e332f8543769d417327424
Akshxar <shakya.ani47@gmail.com>
2024-07-03 22:39:48 +0530

    understanding req.url during servers

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const http = require('http')
 
 const myServer =http.createServer((req,res)=>{
     console.log(req.headers)
-    res.end("Hello ")
+    
+    if(req.url === '/')
+    {
+        res.end("hello")
+    }
+
+    if(req.url === '/about')
+    {
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.end(`<h1>ABOUT PAGE</h1>`)
+    }
+
+    if(req.url === '/contact')
+    {
+        res.setHeader('Content-Type','text/html')
+        res.write('Contact Page')
+        res.end()
+    }
 });
 
 myServer.listen(3000,()=>{
